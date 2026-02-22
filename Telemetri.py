7f50fa9a893aabebc3e742d654a0f8b805f868a3
emirhan5735 <em.ura2023@gmail.com>
2026-02-22 14:40:33 +0300

    Arayüz Tasarımı

---
+++ Telemetri.py
@@ -0,0 +1,19 @@
+class TelemetriVerisi:
+    def __init__(self):
+        self.paket_numarasi = 0
+        self.uydu_statu = "0"
+        self.hata_kodu = "0000"
+        self.gonderme_saati = ""
+        self.basinc = 0.0
+        self.yukseklik = 0.0
+        self.inis_hizi = 0.0
+        self.sicaklik = 0.0
+        self.pil_gerilimi = 0.0
+        self.gps_lat = 0.0
+        self.gps_lon = 0.0
+        self.gps_alt = 0.0
+        self.pitch = 0.0
+        self.roll = 0.0
+        self.yaw = 0.0
+        self.rhrhrh = ""
+        self.takim_no = "71523"
